9ce89baf9500da00b26803b37f3bb50e0842d8d5
Diego Julio Pinto Beltran <127121667+DiegoShuggy@users.noreply.github.com>
2023-07-11 20:53:21 -0400

    Add files via upload

--- vork/urls.py
+++ vork/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('menu.urls')),
+    path('accounts/', admin.site.urls),
     path('api/',include('rest_vork.urls')),
     
 ]
